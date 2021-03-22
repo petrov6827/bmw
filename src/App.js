83cdb31cade3c2cc137d79804da3b4a81fb6ce80
Dmitriy Petrov <d.petrov@aveds.ru>
2021-03-22 15:41:28 +0300

    build

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          йо
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://petrov6827.github.io/portfolio/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          petrov6827.github.io/portfolio/
         </a>
       </header>
     </div>
